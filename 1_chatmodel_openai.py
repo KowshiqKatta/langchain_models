c38600c08d1dc7f0a84890260084336497ff0348
KowshiqKatta <kattamurikowshiq@gmail.com>
2026-06-01 09:08:33 +0530

    langchain models created

---
+++ 1_chatmodel_openai.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model = "gpt-4", temperature = 1.5, max_completion_tokens = 10) # temperature controls randomness. max completion tokens is about number of tokens in the output.
+
+result = model.invoke("write a 5 line poem on cricket")
+
+print(result.content)
